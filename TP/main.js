7ff666de1687f4654aadaa3454bd5af400cb855c
Agustina Farias <agustinabfarias@gmail.com>
2023-06-21 17:30:03 -0300

    json, fetch y correcciones de carrito

--- TP/main.js
+++ TP/main.js
@@ -1,16 +1,15 @@
-//Hola profe, hasta acá pude llegar con mi entrega. Tengo problemas con el storage ya que me marca de entrada que tengo 6 elementos en el carrito. Los cuales son los que estan en el array de productos. Hay algo mal ordenado o me falta cambiar alguna parte especifica del código? Espero sus correcciones, Muchas gracias!!
+//ENTREGA FINAL
 
-//Tercera Pre-Entrega
 
 //productos del e-commerce 
-const stockProductos = [
-  { id: 1, nombre: "Remera Aeropostale", precio: 8000, img: './img/remera-aeropostale.jpg' },
-  { id: 2, nombre: "top-nike", precio: 8500, img:'./img/top-nike.jpg' },
-  { id: 3, nombre: "buzo-levis", precio: 12000, img:'./img/buzo-levis.jpg' },
-  { id: 4, nombre: "campera de jean", precio: 32000, img:'./img/campera-jean.jpg' },
-  { id: 5, nombre: "botas alanya", precio: 18000, img:'./img/botas.jpeg' },
-  { id: 6, nombre: "blazer", precio: 35000, img:'./img/blazer.jpg' },
-];
+// const stockProductos = [
+//   { id: 1, nombre: "Remera Aeropostale", precio: 8000, img: './img/remera-aeropostale.jpg' },
+//   { id: 2, nombre: "top-nike", precio: 8500, img:'./img/top-nike.jpg' },
+//   { id: 3, nombre: "buzo-levis", precio: 12000, img:'./img/buzo-levis.jpg' },
+//   { id: 4, nombre: "campera de jean", precio: 32000, img:'./img/campera-jean.jpg' },
+//   { id: 5, nombre: "botas alanya", precio: 18000, img:'./img/botas.jpeg' },
+//   { id: 6, nombre: "blazer", precio: 35000, img:'./img/blazer.jpg' },
+// ];
 
 const contenedorProductos=document.getElementById("contenedor-productos")
 //boton eliminar
@@ -19,8 +18,12 @@ const vaciar = document.getElementById("eliminar")
 const contadorCarrito = document.getElementById("contadorCarrito");
 
 let carrito = []; 
+let precioTotal = document.getElementById("precioTotal");
+precioTotal.innerText = "0";
+
+
 //carrito en el storage
-localStorage.setItem("carrito", JSON.stringify(stockProductos));
+localStorage.setItem("carrito", JSON.stringify(carrito));
 
 let carritoStorage = JSON.parse(localStorage.getItem("carrito")) || [];
 
@@ -28,39 +31,53 @@ if (!carritoStorage.length) {
   localStorage.setItem("carrito", JSON.stringify([]));
 }
 
-//productos en el document
-stockProductos.forEach((producto) => { 
-  let div = document.createElement("div"); 
-  div.innerHTML = ` 
-    <img src=${producto.img} alt= "">
-    <h3>Nombre: ${producto.nombre}</h3>
-    <b class="precio-producto">$${producto.precio}</b>
-    <button id="agregar${producto.id}" class="boton-agregar">Agregar</button>
-    <hr />
-  `;
-  contenedorProductos.append(div); 
-  
-  const boton =document.getElementById(`agregar${producto.id}`)
-  boton.addEventListener
+let data; 
 
-  boton.addEventListener('click', () => {
-    agregarAlCarrito(producto.id)
-  })
-});
+const stockProductos = async () => {
+  try {
+    const response = await fetch("./data.json");
+    data = await response.json();
+
+    data.forEach((post) => {
+      let div = document.createElement("div"); 
+      div.innerHTML = ` 
+          <img src=${post.img} alt= "">
+          <h3> ${post.nombre}</h3>
+          <b class="precio-producto">$${post.precio}</b>
+          <button id="agregar${post.id}" class="boton-agregar">Agregar</button>
+          <hr />
+          `;
+          contenedorProductos.append(div); 
+
+          const boton =document.getElementById(`agregar${post.id}`)
+        
+          boton.addEventListener('click', () => {
+            agregarAlCarrito(post.id)
+          })
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+stockProductos();
+
+
 
 //agregar productos al carrito
 const agregarAlCarrito = (productoId) => {
-  const item = stockProductos.find((producto) => producto.id === productoId)
+  const item = data.find((post) => post.id === productoId)
   carrito.push(item)
   localStorage.setItem('carrito', JSON.stringify(carrito))
   console.log(carrito)
+  precioTotal.innerText = (parseInt(precioTotal.innerText) + item.precio).toString();
   actualizarCarrito();
 }
 
 //actualizar carrito
 
 const actualizarCarrito = () =>{
-  contadorCarrito.innerHTML = carrito.length
+  contadorCarrito.innerHTML = carrito.length;
 }
 
 // Cargar carrito del localStorage
@@ -68,7 +85,7 @@ carrito = carritoStorage.slice();
 actualizarCarrito();
 console.log(carrito)
 
-// //vaciar el carrito
+//vaciar el carrito
 vaciar.addEventListener("click", () => {
   localStorage.clear();
   console.clear();
